0816addac37176fdf3b8dc196fee2182e2a0d524
Michał Nadolny <mnadolny.laba@solvd.com>
2024-08-22 12:19:15 +0200

    feat: add basic font weights and sizes

--- src/theme.ts
+++ src/theme.ts
@@ -10,7 +10,32 @@ const workSans = Work_Sans({
 
 const theme = createTheme({
   typography: {
-    fontFamily: workSans.style.fontFamily
+    fontFamily: workSans.style.fontFamily,
+    fontSize: 15,
+    h1: {
+      fontWeight: '500',
+      fontSize: 45,
+      lineHeight: '52.79px',
+    },
+    body1: {
+      fontSize: 25,
+      lineHeight: '33.83px',
+      fontWeight: '400'
+    },
+    body2: {
+      fontSize: 15,
+      lineHeight: '17.6px',
+    },
+    caption: {
+      fontSize: 15,
+      lineHeight: '17.6px',
+      fontWeight: '500',
+    },
+    button: {
+      fontSize: 16,
+      lineHeight: '18.77px',
+      fontWeight: '500',
+    }
   }
 });
 
