916ba9a0565c808eeadad4cf2b717e56b7d8b9da
Fabi0o <marfab32@gmail.com>
2024-08-23 21:07:05 +0200

    feat: change button typography and add contrastText color to primary color

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,11 @@
 "use client";
-import { Work_Sans } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
+import { Work_Sans } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
 
 const workSans = Work_Sans({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap'
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const theme = createTheme({
@@ -13,48 +13,48 @@ const theme = createTheme({
     fontFamily: workSans.style.fontFamily,
     fontSize: 15,
     h1: {
-      fontWeight: '500',
+      fontWeight: "500",
       fontSize: 45,
-      lineHeight: '52.79px',
+      lineHeight: "52.79px",
     },
     body1: {
       fontSize: 25,
-      lineHeight: '33.83px',
-      fontWeight: '400'
+      lineHeight: "33.83px",
+      fontWeight: "400",
     },
     body2: {
       fontSize: 15,
-      lineHeight: '17.6px',
-      fontWeight: '300',
+      lineHeight: "17.6px",
+      fontWeight: "300",
     },
     caption: {
       fontSize: 15,
-      lineHeight: '17.6px',
-      fontWeight: '500',
+      lineHeight: "17.6px",
+      fontWeight: "500",
     },
     button: {
       fontSize: 16,
-      lineHeight: '18.77px',
-      fontWeight: '500',
+      lineHeight: "18.77px",
+      fontWeight: "500",
+      textTransform: "none",
     },
   },
   palette: {
     error: {
-      main: '#FE645E',
+      main: "#FE645E",
     },
     text: {
-      primary: '#000000',
-      secondary: '#5C5C5C',
+      primary: "#000000",
+      secondary: "#5C5C5C",
     },
     primary: {
-      main: '#FE645E',
+      main: "#FE645E",
+      contrastText: "#FFFFFF",
     },
     secondary: {
-      main: '#6E314A',
-    }
-  }
+      main: "#6E314A",
+    },
+  },
 });
 
 export default theme;
-
-
