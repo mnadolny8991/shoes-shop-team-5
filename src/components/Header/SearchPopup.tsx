ee219063e83a71debf2a87a3e6a646e6ca31cc99
jomal123a <36798428+jomal123a@users.noreply.github.com>
2024-08-30 11:31:18 +0200

    feat: add popular search terms

--- src/components/Header/SearchPopup.tsx
+++ src/components/Header/SearchPopup.tsx
@@ -1,7 +1,22 @@
-import { Box, Drawer, IconButton, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Drawer, IconButton, List, ListItem, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import Image from "next/image";
 import SearchBar from "../SearchBar/SearchBar";
+
+const popularSearchTerms = [
+  {
+    id: 1,
+    value: "Nike Air Force 1 LV8"
+  },
+  {
+    id: 2,  
+    value: "Nike Air Force 1"
+  },
+  {
+    id: 3,
+    value: "Nike Air Force 1 '07 High"
+  }
+];
 
 type SearchPopupProps = {
   show: boolean;
@@ -34,7 +49,8 @@ export default function SearchPopup({
         flexDirection: "row",
         width: "100%",
         justifyContent: "space-between",
-        padding: "20px",
+        padding: "40px",
+        paddingTop: "45px",
         alignItems: isMobile ? "center" : "start",
       }}>
         {
@@ -47,18 +63,35 @@ export default function SearchPopup({
               alt="website logo" />
           </IconButton>
         }
-        <SearchBar
-          value={searchText}
-          onChange={(val: string) => onTextChange(val)}
-          width={isMobile ? "290px" : "700px"}
-          height={isMobile ? "25px" : "79px"}
-          variant={isMobile ? "popupMobile" : "popupLarge"} />
+        <Stack gap="40px">
+          <SearchBar
+            value={searchText}
+            onChange={(val: string) => onTextChange(val)}
+            width={isMobile ? "290px" : "700px"}
+            height={isMobile ? "25px" : "79px"}
+            variant={isMobile ? "popupMobile" : "popupLarge"} />
+            { !isMobile && 
+            <Stack gap="24px">
+              <Typography sx={{ fontWeight: "500", fontSize: "20px", lineHeight: "23.5px", color: "text.secondary" }}>
+                Popular Search Terms
+              </Typography>
+              {
+                popularSearchTerms.map(term => 
+                  <Typography 
+                    key={term.id}
+                    sx={{ fontWeight: "500", fontSize: "22px", lineHeight: "25.81px", color: "text.primary" }}>
+                    {term.value}
+                  </Typography>
+                )
+              }
+            </Stack>}
+        </Stack>
         <IconButton
           onClick={close}
           sx={{
             color: "#494949",
           }}>
-          <CloseIcon sx={{ width: "15px", height: "15px" }} />
+          <CloseIcon sx={{ width: isMobile ? "15px" : "24px", height: isMobile ? "15px" : "24px" }} />
         </IconButton>
       </Box>
     </Drawer>
