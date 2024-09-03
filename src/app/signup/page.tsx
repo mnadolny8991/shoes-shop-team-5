a55fe52f33c8edbfac1feac646c9cb482458534e
Sambuka <vova.kurtiak@gmail.com>
2024-09-03 12:14:03 +0200

    signup page done, correct project structure

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import theme from "@/theme";
+import { Grid, Box, Typography, useMediaQuery } from "@mui/material";
+import Image from 'next/image'
+import SignupForm from "@/components/forms/SignupForm";
+import SignupSlider from "@/components/sliders/SignupSlider";
+
+
+export default function Signup() {
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const Logo = () => {
+    return (
+      <Image
+        src="/logo.svg"
+        width={isMobile ? 35 : 40}
+        height={isMobile ? 26.5 : 30}
+        alt="shoes shop logo"
+      />
+    )
+  }
+
+  return (
+    <Grid container sx={{ height: "100vh" }}>
+      <Grid
+        xs={12} md={6}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: isMobile ? '20px' : '50px',
+            left: isMobile ? '20px' : '40px',
+          }}
+        >
+          <Logo />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '15px',
+            width: isMobile ? '320px' : '436px',
+          }}
+        >
+          <Typography variant="h1" fontSize={isMobile ? "30px" : "45px"}>
+            Create an account
+          </Typography>
+          <Typography variant="subtitle2">
+            Create an account to get an easy access to your dream shopping
+          </Typography>
+          <SignupForm />
+        </Box>
+      </Grid >
+      {!isMobile && <Grid
+        xs={false}
+        md={6}
+        sx={{
+          backgroundImage: "url(/signup-form-desktop-bg-image.png)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-end',
+          padding: '16px',
+        }}
+      >
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'flex-end',
+            mb: '252px',
+          }}
+        >
+          <SignupSlider />
+        </Box>
+      </Grid>}
+    </Grid>
+  );
+}
---
+++ src/components/forms/SignupForm.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Box, Typography, Link, useTheme, useMediaQuery } from "@mui/material";
+import TextField from "../input/TextField";
+import CustomButton from "../buttons/CustomButton";
+
+export default function SignupForm() {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'))
+
+
+  return (
+    <form>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          maxWidth: isMobile ? '320px' : '436px',
+          gap: '22px',
+        }}
+      >
+        <TextField required name='name' id='name' label='Name' min={8} value={''} onChange={() => { }} />
+        <TextField required name='email' id='email' label='Email' min={8} value={''} onChange={() => { }} />
+        <TextField required name='password' id='password' label='Password' min={8} value={''} onChange={() => { }} />
+        <TextField required name='confirm-password' id='confirm-password' label='Confirm password' min={8} value={''} onChange={() => { }} />
+
+        <Box
+          sx={{
+            marginTop: isMobile ? '29px' : '68px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '16px',
+          }}
+        >
+          <CustomButton size={isMobile ? 's' : 'l'} variant='contained' type='submit'>
+            Sign Up
+          </CustomButton>
+          <Typography
+            variant='caption'>
+            Already have an account?
+            <Link sx={{
+              marginLeft: isMobile ? '5px' : '7px',
+            }} href='/sign-in' color='primary'>Log in</Link>
+          </Typography>
+        </Box>
+      </Box>
+    </form>
+  )
+}
