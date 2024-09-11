6d9b4789feef31377a081f2209209e149478abba
Serhii Lamanov <svlam@ukr.net>
2024-09-11 14:10:18 +0300

    refactor: put MyProductsHeader into the separate file

--- src/app/(header)/(user)/my-products/page.tsx
+++ src/app/(header)/(user)/my-products/page.tsx
@@ -1,64 +1,26 @@
 'use client';
 
 import {
-  Avatar,
   Box,
   Stack,
   Typography,
   useMediaQuery,
   useTheme,
 } from '@mui/material';
-import Image from 'next/image';
-import unsplash from '/public/pawel-czerwinski-unsplash-3.png';
 
 import { products } from '@/mock/products';
 import ProductsGrid from '@/components/products/ProductsGrid';
 import MyProductsEmptyState from '@/components/products/MyProductsEmptyState';
 import AddProductButton from '@/components/products/AddProductButton';
+import MyProductsHeader from '@/components/products/MyProductsHeader';
 
-const MyProductsHeader = () => {
+export default function MyProducts() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
     <>
-      <Box height={{ xs: 132, md: 262 }} position="relative" width="100%">
-        <Image
-          alt="Pawel Czerwinski unsplash-3"
-          src={unsplash}
-          placeholder="blur"
-          quality={100}
-          fill
-          sizes={isMobile ? '100vw' : 'calc(100vw - 440px)'}
-          style={{
-            objectFit: 'cover',
-          }}
-        />
-      </Box>
-      <Box
-        position="relative"
-        top={{ xs: -15, md: -30 }}
-        marginLeft={{ xs: '20px', md: '55px' }}
-        display="flex"
-        gap={{ xs: '13px', md: '26px' }}
-        alignItems="end"
-      >
-        <Avatar
-          alt="Jane Meldrum"
-          src="/jane-meldrum.png"
-          sx={{
-            width: { xs: 64, md: 128 },
-            height: { xs: 64, md: 128 },
-            border: '4px solid white',
-          }}
-        />
-        <Box>
-          <Typography variant="h5" mb={{ xs: '2px', md: '4px' }}>
-            Jane Meldrum
-          </Typography>
-          <Typography variant="subtitle2">1 374 bonus points</Typography>
-        </Box>
-      </Box>
+      <MyProductsHeader />
       <Stack
         direction={'row'}
         justifyContent="space-between"
@@ -69,17 +31,6 @@ const MyProductsHeader = () => {
         <Typography variant="h1">My products</Typography>
         {products && !isMobile && <AddProductButton />}
       </Stack>
-    </>
-  );
-};
-
-export default function MyProducts() {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-
-  return (
-    <>
-      <MyProductsHeader />
       {products ? (
         <>
           <ProductsGrid products={products} />
