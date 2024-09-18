6c4c6db4332521740dd5b03c57d5290679f489f7
Michał Nadolny <mnadolny.laba@solvd.com>
2024-09-18 22:54:11 +0200

    fix: useLocalStorage hydration issues

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,18 +1,28 @@
 'use client';
 import { useEffect, useState } from 'react';
 
-const useLocalStorage = <T>(key: string, initialValue?: T) => {
-  const [data, setData] = useState<T>(() => {
-    const dataJson = localStorage.getItem(key);
-    if (dataJson) {
-      return JSON.parse(dataJson) as T;
-    }
-    return initialValue as T;
-  });
+const useLocalStorage = <T,>(key: string, initialValue: T) => {
+  const [data, setData] = useState<T>(initialValue);
+  const [isMounted, setIsMounted] = useState(false); // Track hydration
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(data));
-  }, [data, key]);
+    setIsMounted(true); // Set mounted to true when the component mounts
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      const dataJson = localStorage.getItem(key);
+      if (dataJson) {
+        setData(JSON.parse(dataJson) as T);
+      }
+    }
+  }, [isMounted, key]);
+
+  useEffect(() => {
+    if (isMounted) {
+      localStorage.setItem(key, JSON.stringify(data));
+    }
+  }, [data, key, isMounted]);
 
   return [data, setData] as const;
 };
