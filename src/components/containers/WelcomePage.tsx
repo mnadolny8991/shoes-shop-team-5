b5b39a7037a3c94fe2befeecfbbae4ee0f07b33f
Michał Nadolny <mnadolny.laba@solvd.com>
2024-09-24 16:50:00 +0200

    feat: add Welcome page for not signed in user

---
+++ src/components/containers/WelcomePage.tsx
@@ -0,0 +1,62 @@
+import { Box, Paper, Stack, Typography } from '@mui/material';
+import { FC } from 'react';
+import CustomButton from '../buttons/CustomButton';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+type WelcomePageProps = {
+
+}
+
+const WelcomePage: FC<WelcomePageProps> = () => {
+  const router = useRouter();
+
+  return (
+    <Box
+      sx={{
+        height: '300px',
+        position: 'relative',
+      }}
+    >
+      <Paper>
+        <Stack
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: { xs: '200px', md: '400px' },
+            alignItems: 'center',
+            gap: '30px',
+          }}
+        >
+          <Typography variant="h1" width="100%" textAlign="center">
+            Welcome
+          </Typography>
+          <Stack 
+            direction="row"
+            gap="15px"
+            width="100%"
+          >
+            <CustomButton 
+              size='m' 
+              variant="contained"
+              onClick={() => signIn('credentials')}
+            >
+              Sign In
+            </CustomButton>
+            <CustomButton 
+              size='m' 
+              variant="outlined"
+              onClick={() => router.push('/catalog')}
+            >
+              Catalog
+            </CustomButton>
+          </Stack>
+        </Stack>
+      </Paper>
+    </Box>
+  )
+}
+
+export default WelcomePage;
