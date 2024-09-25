0199e9ac7463d82e718e0dd4d1e01fe045c11919
Serhii Lamanov <svlam@ukr.net>
2024-09-25 12:43:47 +0300

    fix: products grid margin

--- src/components/products/ProductsGrid.tsx
+++ src/components/products/ProductsGrid.tsx
@@ -14,7 +14,7 @@ export default function ProductsGrid({
   isAdmin = true,
 }: ProductsGridProps) {
   return (
-    <Grid2 container spacing={{ xs: 2, md: 8 }} mx={{ xs: 1, md: 0 }}>
+    <Grid2 container spacing={{ xs: 2, md: 8 }} mx={{ xs: 1, md: -4 }}>
       {products.map((product) => (
         <Grid2 xs={6} sm={4} md={6} lg={4} xl={3} key={product.id}>
           <ProductCard product={product} isAdmin={isAdmin} />
