f8109482d9cafd617c194d85fe47d384573e5183
Serhii Lamanov <svlam@ukr.net>
2024-10-08 11:29:55 +0300

    feat: add pages to navigation

--- src/data/menuItems.tsx
+++ src/data/menuItems.tsx
@@ -2,30 +2,51 @@ import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlin
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import { LoginOutlined } from '@mui/icons-material';
+import SearchFavorite from '@/components/svgIcons/SearchFavorite';
+import HeartSearch from '@/components/svgIcons/HeartSearch';
+import Cube3dScan from '@/components/svgIcons/Cube3dScan';
 
 const getMenuItems = (signedIn: boolean) => [
   {
     id: 0,
     name: 'My Products',
-    icon: <ShoppingBasketOutlinedIcon sx={{ width: '20px', height: '20px' }} />,
+    icon: <ShoppingBasketOutlinedIcon />,
     href: '/my-products',
   },
   {
     id: 1,
+    name: 'Order history',
+    icon: <Cube3dScan />,
+    href: '/order-history',
+  },
+  {
+    id: 2,
+    name: 'My Wishlist',
+    icon: <HeartSearch />,
+    href: '/my-wishlist',
+  },
+  {
+    id: 3,
+    name: 'Recently viewed',
+    icon: <SearchFavorite />,
+    href: '/recently-viewed',
+  },
+  {
+    id: 4,
     name: 'Settings',
-    icon: <SettingsOutlinedIcon sx={{ width: '20px', height: '20px' }} />,
+    icon: <SettingsOutlinedIcon />,
     href: '/settings',
   },
   signedIn
     ? {
-        id: 2,
+        id: 5,
         name: 'Log Out',
-        icon: <LogoutOutlinedIcon sx={{ width: '20px', height: '20px' }} />,
+        icon: <LogoutOutlinedIcon />,
       }
     : {
-        id: 2,
+        id: 5,
         name: 'Log In',
-        icon: <LoginOutlined sx={{ width: '20px', height: '20px' }} />,
+        icon: <LoginOutlined />,
         href: '/auth/sign-in',
       },
 ];
