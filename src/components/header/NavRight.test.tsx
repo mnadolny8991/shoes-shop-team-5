95fa1da95528983c3ca4a518ba1fdb34543751a6
Michał Nadolny <mnadolny.laba@solvd.com>
2024-10-09 10:46:31 +0200

    feat: add all navRight tests

--- src/components/header/NavRight.test.tsx
+++ src/components/header/NavRight.test.tsx
@@ -21,6 +21,16 @@ describe('Right side of the header component', () => {
         alt: 'user avatar',
       },
     });
+    useSessionMock.mockReturnValue({
+      data: {
+        id: 679,
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Njc5LCJpYXQiOjE3MjYwNDY3NTYsImV4cCI6MTcyODYzODc1Nn0.En7ZyMPzfDsSQuCffuraEgpj0LoO10Jt0K-HuOWRfVs',
+        refreshToken: 'aaa',
+        expires: new Date(Date.now() + 2 * 86400).toISOString(),
+      },
+      status: 'authenticated',
+    })
   });
   
   test('show avatar when signed in', async () => {
@@ -45,19 +55,55 @@ describe('Right side of the header component', () => {
     expect(avatarElement).not.toBeInTheDocument();
   });
 
-  test('shows appropriate icons in mobile viewport', async () => {
+  test('shows appropriate icons in the mobile viewport (auth + notauth)', async () => {
     useMediaQueryMock.mockReturnValue(true);
     render(<NavRight />);
 
-    const menu = await screen.findByAltText('menu icon');
+    const menu = screen.getByAltText('menu icon');
     const avatar = screen.queryByAltText('user avatar');
-    const bag = await screen.findByAltText('bag icon');
-    const search = await screen.findByAltText('search icon');
+    const bag = screen.getByAltText('bag icon');
+    const search = screen.getByAltText('search icon');
 
     expect(menu).toBeInTheDocument();
     expect(avatar).not.toBeInTheDocument();
     expect(bag).toBeInTheDocument();
     expect(search).toBeInTheDocument();
+  });
+
+  test('shows appropriate icons in the desktop viewport (auth)', async () => {
+    useMediaQueryMock.mockReturnValue(false);
+    render(<NavRight />);
+
+    const menu = screen.queryByAltText('menu icon');
+    const avatar = await screen.findByAltText('user avatar');
+    const bag = screen.getByAltText('bag icon');
+    const search = screen.getByPlaceholderText('Search');
+
+    expect(menu).not.toBeInTheDocument();
+    expect(avatar).toBeInTheDocument();
+    expect(bag).toBeInTheDocument();
+    expect(search).toBeInTheDocument();
+  });
+
+  test('shows appropriate icons in the desktop viewport (not auth)', async () => {
+    useMediaQueryMock.mockReturnValue(false);
+    useSessionMock.mockReturnValue({
+      data: null,
+      status: 'unauthenticated',
+    })
+    render(<NavRight />);
+
+    const menu = screen.queryByAltText('menu icon');
+    const avatar = screen.queryByAltText('user avatar');
+    const bag = screen.getByAltText('bag icon');
+    const search = screen.getByPlaceholderText('Search');
+    const signIn = screen.getByText('Sign In');
+
+    expect(menu).not.toBeInTheDocument();
+    expect(avatar).not.toBeInTheDocument();
+    expect(bag).toBeInTheDocument();
+    expect(search).toBeInTheDocument();
+    expect(signIn).toBeInTheDocument();
   });
 
   test('shows mobile menu on menu button click', async () => {
@@ -71,5 +117,27 @@ describe('Right side of the header component', () => {
 
     const popup = await screen.findByTestId('menuPopup');
     expect(popup).toBeInTheDocument();
+  });
+
+  test('shows search popup on search click (desktop)', async () => {
+    useMediaQueryMock.mockReturnValue(false);
+    render(<NavRight />);
+
+    const search = screen.getByPlaceholderText('Search');
+    fireEvent.click(search);
+    const pop = screen.getByText('Popular Search Terms');
+
+    expect(pop).toBeInTheDocument();
+  });
+
+  test('shows search popup on search click (mobile)', async () => {
+    useMediaQueryMock.mockReturnValue(true);
+    render(<NavRight />);
+
+    const search = screen.getByAltText('search icon');
+    fireEvent.click(search);
+    const input = screen.getByPlaceholderText('Search');
+
+    expect(input).toBeInTheDocument();
   });
 });
