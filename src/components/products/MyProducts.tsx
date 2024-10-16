d02a94a58f33398489bb8b8aebcfcfe84d9b6e78
Fabi0o <marfab32@gmail.com>
2024-10-16 19:17:43 +0200

    feat: add pagination on the client side

--- src/components/products/MyProducts.tsx
+++ src/components/products/MyProducts.tsx
@@ -1,24 +1,43 @@
 'use client';
-import { Box, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
+import {
+  Box,
+  IconButton,
+  Stack,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import ProductsGrid from '@/components/products/ProductsGrid';
 import MyProductsEmptyState from '@/components/products/MyProductsEmptyState';
 import AddProductButton from '@/components/products/AddProductButton';
 import ProductsHeader from '@/components/products/ProductsHeader';
 import { useSession } from 'next-auth/react';
 import { useProductsByUserId } from '@/hooks/useProductsByUserId';
-
-type MyProductsProps = {};
+import { useSearchParams, useRouter } from 'next/navigation';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { mapProductList } from '@/mappers/productMappers';
 
 export default function MyProducts() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const page = searchParams.get('page')
+    ? parseInt(searchParams.get('page') as string)
+    : 1;
   const { data: session } = useSession();
-  const { data, isLoading } = useProductsByUserId(
+  const { data, isLoading, isSuccess } = useProductsByUserId(
     session?.id!,
     session?.accessToken!,
-    1,
+    page,
     1
   );
+
+  const products = isSuccess ? mapProductList(data) : [];
+  const hasNextPage = isSuccess
+    ? data.meta.pagination.page < data.meta.pagination.pageCount
+    : false;
 
   return (
     <>
@@ -37,7 +56,21 @@ export default function MyProducts() {
         '...loading'
       ) : data ? (
         <>
-          <ProductsGrid products={data} />
+          <ProductsGrid products={products} />
+          <Stack direction="row" justifyContent="center" marginBottom="20px">
+            <IconButton
+              onClick={() => router.push(`/my-products?page=${page - 1}`)}
+              disabled={page === 1}
+            >
+              <ArrowBackIosIcon />
+            </IconButton>
+            <IconButton
+              onClick={() => router.push(`/my-products?page=${page + 1}`)}
+              disabled={!hasNextPage}
+            >
+              <ArrowForwardIosIcon />
+            </IconButton>
+          </Stack>
           {isMobile && (
             <Box
               height="80px"
