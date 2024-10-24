22ea40d9c273ae230e4154f49a980aafdd966d6e
Michał Nadolny <mnadolny.laba@solvd.com>
2024-10-24 16:27:22 +0200

    feat: input field container test

---
+++ src/components/containers/InputFieldContainer.test.tsx
@@ -0,0 +1,29 @@
+import { render } from '@/testing/testUtils';
+import ImageContainer from './ImageContainer';
+import { screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import InputFieldContainer, { InputProps } from './InputFieldContainer';
+
+describe('Input field container', () => {
+  test('renders all elements correctly', () => {
+    const props = {
+      required: true,
+      label: 'example label',
+      id: '123',
+      error: 'error',
+      width: 100,
+    } as InputProps;
+
+    render(
+      <InputFieldContainer {...props}><p>children</p></InputFieldContainer>
+    );
+
+    const label = screen.getByText(props.label);
+    expect(label).toBeInTheDocument();
+    expect(label).toHaveAttribute('for', '123');
+    const requiredStar = screen.getByText('*');
+    expect(requiredStar).toBeInTheDocument();
+    const child = screen.getByText('children');
+    expect(child).toBeInTheDocument();
+  });
+});
