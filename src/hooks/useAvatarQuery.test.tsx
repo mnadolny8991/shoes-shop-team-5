1b2dc779c2cb9df8d9d5d1eaa2d5c5be9831e072
Michał Nadolny <mnadolny.laba@solvd.com>
2024-10-24 12:26:34 +0200

    feat: avatar query hook tests

--- src/hooks/useAvatarQuery.test.tsx
+++ src/hooks/useAvatarQuery.test.tsx
@@ -1,15 +1,14 @@
 'use client';
-import { fetchProductsByFiltersAndName } from '@/lib/api/fetchProducts';
-import apiResponse from '@/testing/mocks/mockProductsByUserId';
+
+import { apiResponse } from '@/testing/mocks/mockUserDataWithAvatar';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { renderHook, waitFor } from '@testing-library/react';
 import { useAvatarQuery } from './useAvatarQuery';
-import { Filters } from '@/context/SearchContext';
 import { SessionProvider, useSession } from 'next-auth/react';
+import { getUserDataWithAvatar } from '@/lib/api/fetchUserData';
+import { Session } from 'next-auth';
 
-jest.mock('../lib/api/fetchProducts', () => ({
-  fetchProductsByFiltersAndName: jest.fn(),
-}));
+jest.mock('../lib/api/fetchUserData');
 
 describe('use avatar query hook', () => {
   const queryClient = new QueryClient({
@@ -22,11 +21,9 @@ describe('use avatar query hook', () => {
 
   const wrapper = ({ children }: { children: React.ReactNode }) => {
     return (
-      <SessionProvider>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
     );
   }
 
@@ -35,7 +32,42 @@ describe('use avatar query hook', () => {
     jest.clearAllMocks();
   });
 
-  test('fetches user avatar given a session', () => {
-    
+  test('fetches user avatar when authenticated', async () => {
+    (getUserDataWithAvatar as jest.Mock)
+      .mockResolvedValue(apiResponse);
+
+    const { result } = renderHook(() => useAvatarQuery(), { wrapper });
+    expect(result.current.status).toBe('pending');
+
+    await waitFor(() => expect(result.current.data).toStrictEqual({
+      name: 'Michal Nadolny',
+      src: 'https://res.cloudinary.com/devc11z9p/image/upload/v1729760796/avatar_image_4f813fc71f.png',
+      alt: 'your Avatar',
+    }));
+  });
+
+  test('have error status when fetch rejects', async () => {
+    (getUserDataWithAvatar as jest.Mock)
+      .mockRejectedValue(new Error('error'));
+
+    const { result } = renderHook(useAvatarQuery, { wrapper });
+    expect(result.current.status).toBe('pending');
+
+    await waitFor(() => expect(result.current.status).toBe('error'));
+  });
+
+  test('not load avatar when not authenticated', async () => {
+    (useSession as jest.Mock)
+      .mockReturnValue({
+        data: undefined,
+        status: 'unauthentiated',
+      });
+    (getUserDataWithAvatar as jest.Mock)
+      .mockResolvedValue(apiResponse);
+
+    const { result } = renderHook(useAvatarQuery, { wrapper });
+    expect(result.current.status).toBe('pending');
+
+    await waitFor(() => expect(result.current.status).toBe('pending'));
   });
 });
