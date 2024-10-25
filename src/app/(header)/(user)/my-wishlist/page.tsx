2375f5151749cb48cc4c42a2a30859149de51f81
Serhii Lamanov <svlam@ukr.net>
2024-10-25 14:04:47 +0300

    feat: add notifications add co cart and remove from wishlist

--- src/app/(header)/(user)/my-wishlist/page.tsx
+++ src/app/(header)/(user)/my-wishlist/page.tsx
@@ -1,11 +1,22 @@
 'use client';
-import { Typography } from '@mui/material';
+import { Alert, Snackbar, Typography } from '@mui/material';
 import ProductsGrid from '@/components/products/ProductsGrid';
 import useProducts from '@/hooks/useProducts';
 import { useWishlist } from '@/context/WishlistContext';
+import { useEffect, useRef, useState } from 'react';
+import { Product } from '@/types/product';
 
 export default function MyWishlist() {
   const products = useProducts(useWishlist);
+  const prevProductsRef = useRef(products);
+  const [deletedProduct, setDeletedProduct] = useState<Product | null>(null);
+  useEffect(() => {
+    const deleted = prevProductsRef.current.find(
+      (product) => !products.includes(product)
+    );
+    if (deleted) setDeletedProduct(deleted);
+    prevProductsRef.current = products;
+  }, [products]);
 
   return (
     <>
@@ -17,6 +28,21 @@ export default function MyWishlist() {
         My Wishlist
       </Typography>
       <ProductsGrid products={products} isAdmin={false} isInWishlist={true} />
+      <Snackbar
+        key={deletedProduct?.id}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={!!deletedProduct}
+        autoHideDuration={2000}
+        onClose={() =>
+          setDeletedProduct((deleted) =>
+            deleted === deletedProduct ? null : deleted
+          )
+        }
+      >
+        <Alert severity="info">
+          {deletedProduct?.name} removed from wishlist
+        </Alert>
+      </Snackbar>
     </>
   );
 }
