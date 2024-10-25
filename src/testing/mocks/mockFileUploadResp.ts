415819af2041ae9f5d226a3b1654d502a54430d4
Michał Nadolny <mnadolny.laba@solvd.com>
2024-10-25 12:10:18 +0200

    feat: add product mutation test

---
+++ src/testing/mocks/mockFileUploadResp.ts
@@ -0,0 +1,44 @@
+const apiResponse = JSON.parse(
+  `[
+      {
+          "id": 6107,
+          "name": "shoes-photo.png",
+          "alternativeText": null,
+          "caption": null,
+          "width": 936,
+          "height": 1170,
+          "formats": {
+              "thumbnail": {
+                  "ext": ".png",
+                  "url": "https://res.cloudinary.com/devc11z9p/image/upload/v1729846220/thumbnail_shoes_photo_d33752310b.webp",
+                  "hash": "thumbnail_shoes_photo_d33752310b",
+                  "mime": "image/png",
+                  "name": "thumbnail_shoes-photo.png",
+                  "path": null,
+                  "size": 0.99,
+                  "width": 125,
+                  "height": 156,
+                  "provider_metadata": {
+                      "public_id": "thumbnail_shoes_photo_d33752310b",
+                      "resource_type": "image"
+                  }
+              }
+          },
+          "hash": "shoes_photo_d33752310b",
+          "ext": ".png",
+          "mime": "image/png",
+          "size": 24.36,
+          "url": "https://res.cloudinary.com/devc11z9p/image/upload/v1729846220/shoes_photo_d33752310b.webp",
+          "previewUrl": null,
+          "provider": "cloudinary",
+          "provider_metadata": {
+              "public_id": "shoes_photo_d33752310b",
+              "resource_type": "image"
+          },
+          "createdAt": "2024-10-25T08:50:20.801Z",
+          "updatedAt": "2024-10-25T08:50:20.801Z"
+      }
+  ]`
+);
+
+export default apiResponse;
